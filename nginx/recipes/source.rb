920ba3ddb8134aaecb49cfbb2a3d300c39bce81f
Omar Karim <omkarim@gmail.com>
2010-10-17 00:54:15 -0400

    Removed cookbook_file directive

--- nginx/recipes/source.rb
+++ nginx/recipes/source.rb
@@ -124,10 +124,10 @@ template "nginx.conf" do
   notifies :restart, resources(:service => "nginx"), :immediately
 end
 
-cookbook_file "#{node[:nginx][:dir]}/mime.types" do
-  source "mime.types"
-  owner "root"
-  group "root"
-  mode "0644"
-  notifies :restart, resources(:service => "nginx"), :immediately
-end
+# cookbook_file "#{node[:nginx][:dir]}/mime.types" do
+#   source "mime.types"
+#   owner "root"
+#   group "root"
+#   mode "0644"
+#   notifies :restart, resources(:service => "nginx"), :immediately
+# end
