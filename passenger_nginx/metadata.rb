aa3c1bded47e1579198443eac15f7c9c0edaadf5
Omar Karim <omkarim@gmail.com>
2010-10-17 02:26:14 -0400

    Renamed attributes

--- passenger_nginx/metadata.rb
+++ passenger_nginx/metadata.rb
@@ -4,8 +4,8 @@ license           "Apache 2.0"
 description       "Installs and configures nginx"
 version           "0.14.3"
 
-recipe "nginx", "Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available"
-recipe "nginx::source", "Installs nginx from source and sets up configuration with Debian apache style with sites-enabled/sites-available"
+recipe "passenger_nginx", "Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available"
+recipe "passenger_nginx::source", "Installs nginx from source and sets up configuration with Debian apache style with sites-enabled/sites-available"
 
 %w{ ubuntu debian centos redhat fedora }.each do |os|
   supports os
